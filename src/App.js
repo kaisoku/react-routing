8426e35d45225a491dfd1bc3b592cb4c23530a55
Kaisoku <khuss063@gmail.com>
2023-05-10 21:28:15 +0200

    dev: dynamic routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetail";
 
 /* const routeDefinitions = createRoutesFromElements(
   <Route>
@@ -20,6 +21,7 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductsPage /> },
+      { path: "/products/:id", element: <ProductDetailPage /> },
     ],
   },
 ]);
